dd492a55d787114caecc0ccfb805b5c29d76463f
michael kalango <michaelkalango@gmail.com>
2022-10-23 08:43:44 +0100

    feat: App Setup

---
+++ lib/core/app/app.dart
@@ -0,0 +1,32 @@
+import 'package:sqflite_migration_service/sqflite_migration_service.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:stacked_themes/stacked_themes.dart';
+
+import '../../features/home/home_view.dart';
+import '../../features/splash/splash_view.dart';
+import '../services/database_service.dart';
+import '../services/dio_service.dart';
+
+@StackedApp(
+  routes: [
+    AdaptiveRoute(page: SplashView, initial: true),
+    AdaptiveRoute(page: HomeView),
+  ],
+  dependencies: [
+    LazySingleton(classType: NavigationService),
+    LazySingleton(classType: SnackbarService),
+    LazySingleton(
+      classType: ThemeService,
+      resolveUsing: ThemeService.getInstance,
+    ),
+    LazySingleton(classType: DatabaseMigrationService),
+    LazySingleton(classType: DatabaseService),
+    LazySingleton(
+      classType: DioHttpServiceImpl,
+      asType: DioHttpService,
+    ),
+  ],
+  logger: StackedLogger(),
+)
+class AppSetup {}
