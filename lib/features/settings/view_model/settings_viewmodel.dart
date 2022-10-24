71f3a96df0eab873ddf057f5ea7470cd9b7e6bac
michael kalango <michaelkalango@gmail.com>
2022-10-24 17:55:31 +0100

    chore: settings

--- lib/features/settings/view_model/settings_viewmodel.dart
+++ lib/features/settings/view_model/settings_viewmodel.dart
@@ -1,3 +1,24 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_themes/stacked_themes.dart';
 
-class SettingsViewModel extends BaseViewModel {}
+import '../../../core/app/app.locator.dart';
+
+class SettingsViewModel extends BaseViewModel {
+  final _themeService = locator<ThemeService>();
+
+  List<String> tempList = ['C', 'F'];
+
+  String? _temperature = '';
+  String? get temperature => _temperature;
+
+  void actionSwitchTemp(String? value) {
+    _temperature = value;
+    notifyListeners();
+  }
+
+  void actionSwitchTheme() {
+    _themeService.toggleDarkLightTheme();
+  }
+
+  bool get isDark => _themeService.isDarkMode;
+}
